fe6ddd4dd496040a3ba69f63b0bc3bd4e8dac3e2
clanblog <82025504+clanblog@users.noreply.github.com>
2021-09-19 16:49:07 +0700

    Create scripdua.js

---
+++ scripdua.js
@@ -0,0 +1,35 @@
+var _0xda2f = ['1R 1T(T){j=1K?1E.2r((Z.i+1)*1E.2M()):0,k=1z 1y,2I<=T.g.h.i?18=2R:18=T.g.h.i,1k.1l(\'<e f="2D-2q"><e f="2E 1p-2s">\');z(l G=0;G<18;G++){z(l H,P,N=T.g.h[G],L=N.1g.$t,K="",U=0;U<T.g.h[G].y.i;U++){K=K+\'<a o="/1r/1u/\'+T.g.h[G].y[U].V+\'?1w-1o=6">\'+T.g.h[G].y[U].V+"</a>",P=\'<a o="/1r/1u/\'+T.g.h[G].y[U].V+\'?1w-1o=6">\'+T.g.h[G].y[U].V+"</a>"}m(G==T.g.h.i){W}z(l M=0;M<N.n.i;M++){m("1U"==N.n[M].1t){H=N.n[M].o;W}}z(M=0;M<N.n.i;M++){m("1V"==N.n[M].1t&&"2g/1s"==N.n[M].2f){N.n[M].1g.X(" ")[0];W}}m("1j"1v N){l R=N.1j.$t}A{m("1q"1v N){R=N.1q.$t}A{R=""}}Y=N.21.$t,j>Z.i-1&&(j=0),k[G]="",s=R,a=s.14("<k"),b=s.14(\'13="\',a),O=s.14(\'"\',b+5),d=s.2k(b+5,O-b-5),-1!=a&&-1!=b&&-1!=O&&""!=d&&(k[G]=0==G?\'<k f="1p-25" 1c-16="26" 1c-15="27" f="24-k" 13="\'+d+\'"/>\':\'<k f="23" 1c-15="1x" 13="\'+d+\'" 16="1Z"/>\');z(l S=[1,2,3,4,5,6,7,8,9,10,11,12],J=["2h","2e","2d","2a","1Y","2c","2i","1S","1O","1N","1Q","1M"],Q=Y.X("-")[2].1L(0,2),F=Y.X("-")[1],B=Y.X("-")[0],E=0;E<S.i;E++){m(1W(F)==S[E]){F=J[E];W}}l O=T.g.h[G].1j.$t,I=(E=$("<e>").1s(O)).1X("k:2b").2j("13"),D=T.g.h[G].1h$1e.v;-1!=D.14("k.2N.1i")&&(D=T.g.h[G].1h$1e.v.1n("2K","2L")),D=D.1n("/2J-c/","/1f/");O="1F://1.1B.1D.1i/-2H/2O/2V/2W/1f/w-2T-2S-2P.1A";m(2Q 0===I){m(T.g.h[G].1h$1e){l C=\'<a o="\'+H+\'" f="x-w" 17="u:v(\'+D+\') 19-1d r r;u-1m: 1J;16: 1x%;15: 2m;1b:1a;w-2n: -2t-2u-2A;"/>\'}A{C=\'<a o="\'+H+\'" f="2B x-w" 17="u:v(\'+O+\') 19-1d r r; 1b:1a;1I:1H(1G);"/>\'}}A{C=\'<a o="\'+H+\'" f="x-w" 17="u:v(\'+D+\') 19-1d r r;u-1m: 1J; 1b:1a;1I:1H(1G);"/>\'}l q=\'<e f="x-2x"><e f="x-2w"><e f="2v 2y">\'+C+\'</e><e f="2z">\'+C+\'</e><e f="p-2C"><e f="p-2l"><e f="p-2o"><e f="p-2p">\'+P+\'</e></e><1C f="p-1g"><a o="\'+H+\'">\'+L+\'</a></1C><e f="p-2G">\'+(Q+" "+F+" "+B)+\'</e></e></e><e f="2U"></e></e></e>\';1k.1l(q),j++}1k.1l("</e></e>")};Z=1z 1y,Z[0]="1F://3.1B.1D.1i/-1P/29/28/22/1f/20.1A",1K=2F;', 
+"|", "split", "||||||||||||||div|class|feed|entry|length||img|var|if|link|href|post||center|||background|url|image|slider|category|for|else|||||||||||||||||||||term|break|split|postdate|imgr||||src|indexOf|height|width|style|maxpost|no|block|display|min|repeat|thumbnail|s400|title|media|com|content|document|write|size|replace|results|owl|summary|search|html|rel|label|in|max|100|Array|new|png|bp|h2|blogspot|Math|http|6px|blur|filter|cover|showRandomImg|substring|December|October|September|zP87C2q9yog|November|function|August|postarea4|alternate|replies|parseInt|find|May|150|picture_not_available|published|AIyPvrpGLn8|alignright|odd|lazy|620|240|AAAAAAAAE5k|UVopoHY30SI|April|first|June|March|February|type|text|January|July|attr|substr|inner|280px|rendering|meta|tag|wrapper|floor|carousel|webkit|optimize|thumb|wrapp|item|overlay|covert|contrast|noimg|descript|ct|owl_carouselle|true|date|S5pmP6Y0KUs|postnum4|s72|default|maxresdefault|random|youtube|V6CsQEqKNGI|4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef|void|postnum5|found|not|clear|AAAAAAAAEFY|LZigJ20wKVkpzFLJSuzvjJDkC9z9IFy5wCK4B", 
+"", "fromCharCode", "replace", "\\w+", "\\b", "g"];
+eval(function(app, a, c, k, e, r) {
+  /**
+   * @param {number} c
+   * @return {?}
+   */
+  e = function(c) {
+    return (c < a ? _0xda2f[4] : e(parseInt(c / a))) + ((c = c % a) > 35 ? String[_0xda2f[5]](c + 29) : c.toString(36));
+  };
+  if (!_0xda2f[4][_0xda2f[6]](/^/, String)) {
+    for (; c--;) {
+      r[e(c)] = k[c] || e(c);
+    }
+    /** @type {!Array} */
+    k = [function(cmd) {
+      return r[cmd];
+    }];
+    /**
+     * @return {?}
+     */
+    e = function() {
+      return _0xda2f[7];
+    };
+    /** @type {number} */
+    c = 1;
+  }
+  for (; c--;) {
+    if (k[c]) {
+      app = app[_0xda2f[6]](new RegExp(_0xda2f[8] + e(c) + _0xda2f[8], _0xda2f[9]), k[c]);
+    }
+  }
+  return app;
+}(_0xda2f[0], 62, 183, _0xda2f[3][_0xda2f[2]](_0xda2f[1]), 0, {}));
